f2b35bf3db88b2e79d5d2ef6902a7f86ed81ff9c
arodri04 <augustine.rodriguez55@gmail.com>
2024-10-23 10:35:23 -0600

    up to remiving outliers

--- Chapter 15/scatter_squares.py
+++ Chapter 15/scatter_squares.py
@@ -10,5 +10,27 @@
 #Imports
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+#calculating Data automatically
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+
+plt.scatter(x_values, y_values, s=40)
+
+#Set Title and Labels
+plt.title("Square Numbers", fontsize = 24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Square of Value", fontsize = 14)
+
+#Set the range 
+plt.axis([0, 1100, 0, 1100000])
+
+
+#Set tick size of labels
+plt.tick_params(axis='both', which='major', labelsize = 14)
+
+#This wasn't in the book but it fixes things showing up in 
+# scinotation so it'll look like the picture.
+plt.ticklabel_format(style='plain')
+
 plt.show()
