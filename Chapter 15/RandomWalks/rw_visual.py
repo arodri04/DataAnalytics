ba8dc0bc31eb09c2c69b22031c33f63b504aadc9
arodri04 <augustine.rodriguez55@gmail.com>
2024-10-25 14:51:53 -0600

    Random walk

---
+++ Chapter 15/RandomWalks/rw_visual.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+#keep making new walks
+while True:
+    #make a random walk and plot the points
+    rw = RandomWalk()
+    rw.fill_walk()
+
+
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
