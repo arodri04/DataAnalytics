79798db6611d4e216de5d24e92ef1e29df002a3f
arodri04 <augustine.rodriguez55@gmail.com>
2024-10-29 15:13:10 -0600

    Finished randomwalk, needs unittests now

--- Chapter 15/RandomWalks/rw_visual.py
+++ Chapter 15/RandomWalks/rw_visual.py
@@ -4,11 +4,23 @@ from random_walk import RandomWalk
 #keep making new walks
 while True:
     #make a random walk and plot the points
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
+    #Set size of the plotting window
+    plt.figure(dpi=128, figsize=(10,6))
+    
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    # plt.scatter(rw.x_values, rw.y_values, s=15)
+    #Emphasize the first and last
+    plt.scatter(0, 0, c="green", edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
+    #remove axes #plt.axes() did not work needed to swap to gca
+    plt.gca().get_xaxis().set_visible(False)
+    plt.gca().get_yaxis().set_visible(False)
 
-    plt.scatter(rw.x_values, rw.y_values, s=15)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
